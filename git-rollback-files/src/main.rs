35e32b5a0f5857e9321b4b88d78f33dc37b9ae9f
Jonathan Fast <128733927+jfastwhiskerlabs@users.noreply.github.com>
2024-10-21 15:42:59 -0400

    add rollback rs

--- git-rollback-files/src/main.rs
+++ git-rollback-files/src/main.rs
@@ -1,7 +1,8 @@
 #![allow(dead_code)]
 
-use std::error::Error;
-use clap::{Parser};
+use std::{error::Error, path::{Path, PathBuf}};
+use clap::Parser;
+use crossbeam_channel::unbounded;
 use crate::cancellation::spawn_task_with_cancellation;
 
 mod git;
@@ -13,14 +14,22 @@ mod compare;
 mod algorithm;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let (_, cancellation_channel) = unbounded();
+    // let args = cli::CliArgs::parse();
+    // let repo_path = args.repo_folder.as_ref();
+    // let source_branch = args.source_branch.as_ref();
+    // let target_branch = args.target_branch.as_ref();
+    // let excluded_folders: Vec<PathBuf> = args.excluded_folder.iter().map(|x| Path::new(x).to_path_buf()).collect();
+    // let compare_mode = args.compare_mode;
+    let repo_path = Path::new("C:\\Repos\\Fire3");
+    let source_branch = "core/power-aware-api-changes";
+    let target_branch = "core/staging";
+    let mut excluded_folders: Vec<PathBuf> = Vec::new();
+    excluded_folders.push(Path::new("core/api/PowerAware.Api").to_path_buf());
+    let compare_mode = models::CompareMode::ChooseDestination;
+    algorithm::compare_and_merge_changes(repo_path, source_branch, target_branch, excluded_folders, compare_mode, cancellation_channel)?;
+    println!("Rollback process completed. Changes staged but not committed.");
     spawn_task_with_cancellation(|cancellation_channel| {
-        let args = cli::CliArgs::parse();
-        let repo_path = args.repo_folder.as_ref();
-        let source_branch = args.source_branch.as_ref();
-        let target_branch = args.target_branch.as_ref();
-        let excluded_folders = args.excluded_folder;
-        algorithm::compare_and_merge_changes(repo_path, source_branch, target_branch, excluded_folders, cancellation_channel)?;
-        println!("Rollback process completed. Changes staged but not committed.");
         Ok(())
     })?;
     Ok(())
